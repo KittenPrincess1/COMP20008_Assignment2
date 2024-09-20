5a27789794c490c2b113cc54516dccca2761d063
KittenPrincess1 <kittenprinces@gmail.com>
2024-09-20 11:14:24 +1000

    Added seperate files to be used for each group

---
+++ Minors.py
@@ -0,0 +1,2 @@
+def task3():
+    print("Minors")
--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import COMP20008_Assignment2.Elderly as Elderly, COMP20008_Assignment2.Minors as Minors, COMP20008_Assignment2.Racial_Minorities as Racial_Minorities, COMP20008_Assignment2.Disadvantaged_People as Disadvantaged_People
+
 def main():
-    print("Hello world")
+    Disadvantaged_People.task1
+
+    Elderly.task2
+
+    Minors.task3
+
+    Racial_Minorities.task4
+
+
     return
