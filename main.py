134481b22701bab698f9ecb2af48bd9b93a1dc6c
KittenPrincess1 <kittenprinces@gmail.com>
2024-09-21 14:59:16 +1000

    Fixed an issue with importing files

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import COMP20008_Assignment2.Elderly as Elderly, COMP20008_Assignment2.Minors as Minors, COMP20008_Assignment2.Racial_Minorities as Racial_Minorities, COMP20008_Assignment2.Disadvantaged_People as Disadvantaged_People
+import Elderly, Minors, Racial_Minorities, Disadvantaged_People
 
 def main():
     Disadvantaged_People.task1
@@ -8,6 +8,4 @@ def main():
     Minors.task3
 
     Racial_Minorities.task4
-
-
     return
