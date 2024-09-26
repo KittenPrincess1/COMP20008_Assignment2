07393dec467cdb340a07b2680b0f7fb6b4e6059c
KittenPrincess1 <kittenprinces@gmail.com>
2024-09-26 21:46:43 +1000

    implemented preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,19 @@ def get_data(file_name):
     with open(file_name, 'r') as in_file:
         communities = pd.read_csv(in_file)
 
+    # add in any rows that you want to analyse as integers
+    integer_rows = [
+        "IRSD (avg)", "Aboriginal or Torres Strait Islander, persons", "Born overseas, persons", 
+        "Born in non-English speaking country, persons", "Homelessness", "Centrelink Offices", "Medicare Offices",
+        "Equivalent household income <$600/week", "Personal income <$400/week, persons"
+        ]
+
+    for row in integer_rows:
+        communities[row] = communities[row].apply(standardise)
+        communities[row] = communities[row].astype(int)
+
+    return communities
+
 def standardise(s):
     try:
         return int(s)
